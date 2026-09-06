1378166f1f4affc6e140f3d3efdf70e0dc387ba4
gildaspro5613-code <gildaspro5613@gmail.com>
2026-09-06 06:02:05 +0200

    feat(magicq): export lighting adapter surface

--- src/lib/connectors/internal/chamsys/magicq/index.ts
+++ src/lib/connectors/internal/chamsys/magicq/index.ts
@@ -11,6 +11,21 @@ export {
 } from "./crep";
 
 export {
+  MAGICQ_CAPABILITIES,
+  MagicQConnectorDefinition,
+  type MagicQConnectorDefinitionShape,
+} from "./definition";
+
+export { MagicQConnectorAdapter } from "./adapter";
+
+export type {
+  MagicQFixtureIntensityRequest,
+  MagicQFixtureIntensityResult,
+  MagicQFixtureTargetResolver,
+  ResolvedMagicQFixtureTarget,
+} from "./types";
+
+export {
   MagicQCrepTransport,
   type MagicQCrepTransportConfig,
   type MagicQIntensityOperation,
