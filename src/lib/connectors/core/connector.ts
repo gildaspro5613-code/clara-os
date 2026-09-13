40f12bf39869aaf0a7eb31b196e784bb34b2becd
gildaspro5613-code <gildaspro5613@gmail.com>
2026-09-13 21:57:52 +0200

    refactor(connectors): merge executable connector contract

--- src/lib/connectors/core/connector.ts
+++ src/lib/connectors/core/connector.ts
@@ -11,6 +11,8 @@
  */
 
 import { ConnectorContext } from "./connector-context";
+import { ConnectorEvent } from "./connector-event";
+import { ConnectorResult } from "./connector-result";
 
 /**
  * Base connector.
@@ -46,6 +48,11 @@ export interface Connector {
    * Connector availability.
    */
   enabled: boolean;
+
+  /**
+   * Executes one connector capability.
+   */
+  execute(event: ConnectorEvent): Promise<ConnectorResult>;
 
 }
 
