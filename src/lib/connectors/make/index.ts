8f9ba658213cfdb8b24069c8e534fe8277501c7e
gildaspro5613-code <gildaspro5613@gmail.com>
2026-09-18 06:43:41 +0200

    feat(make): export executable connector

--- src/lib/connectors/make/index.ts
+++ src/lib/connectors/make/index.ts
@@ -1,5 +1,6 @@
 export { MakeConnectorAdapter } from "./adapter";
 export type { MakeCapabilityInput, MakeCapabilityResult } from "./adapter";
+export { MakeExecutableConnector } from "./make-executable-connector";
 export { MakeWebhookClient, MakeWebhookError } from "./client";
 export { MAKE_CAPABILITIES, MakeConnectorDefinition } from "./definition";
 export type * from "./types";
